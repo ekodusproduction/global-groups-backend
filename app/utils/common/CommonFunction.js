194c29ef3b070a4a66a2047f466732918a44a708
Tharmahto <dmahato@ekodusinc.com>
2023-09-26 19:17:37 +0530

    test

--- app/utils/common/CommonFunction.js
+++ app/utils/common/CommonFunction.js
@@ -62,7 +62,7 @@ const validateAndProcessFile = (file, allowedFileTypes = ['.jpg', '.jpeg', '.png
   let errors = []
   const maxFileSize = size * 1024 * 1024; // 10 MB
   const fileExtension = file.fieldname.toLowerCase().substring(file.fieldname.lastIndexOf('.'));
-  console.log("fileExtension", path.extname(file.fieldname))
+  console.log("fileExtension", path.extname(file.originalname))
   if (!allowedFileTypes.includes(fileExtension)) {
     errors.push(`Invalid file type: ${fileExtension} for ${file?.fieldname} Required type is ${JSON.stringify(allowedFileTypes)}`)
     return errors
